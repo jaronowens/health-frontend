806ee3666aac0baa0e3a21a5a51556d3ed8955f4
Jaron Owens <jaroncowens@student.midlandstech.edu>
2022-10-20 17:59:53 -0400

    add routing

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
+import PatientList from '../patientList/PatientList';
+
 function App() {
   return (
     <div>
+      <Routes>
+        <Route path="/" element={<PatientList />}/>
+      </Routes>
     </div>
   );
 }
