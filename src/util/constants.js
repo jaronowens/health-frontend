945091432cb16fad833d7552307200d39e91f919
Jaron Owens <jaroncowens@student.midlandstech.edu>
2022-10-20 17:59:35 -0400

    create constants for endpoints

---
+++ src/util/constants.js
@@ -0,0 +1,7 @@
+
+const BASE_URL = 'http://localhost:8080';
+
+const CONTEXT_PATIENTS = '/patients';
+const CONTEXT_ENCOUNTERS = '/encounters';
+
+export { BASE_URL, CONTEXT_ENCOUNTERS, CONTEXT_PATIENTS };
